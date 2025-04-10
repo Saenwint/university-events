c385f89af059190ab5027bc6cd89b855c69b2cf1
saenwint <burntoutblunt@mail.ru>
2025-04-10 11:09:17 +0000

    Шаблон для представления списка мероприятий

--- server/djangoapp/users/admin.py
+++ server/djangoapp/users/admin.py
@@ -3,14 +3,29 @@ from django.contrib.auth.admin import UserAdmin
 
 from users.models import User
 
-# Register your models here.
-@admin.register(User)
 class CustomUserAdmin(UserAdmin):
-    list_display = (
-        'email', 'first_name', 
-        'last_name', 'is_admin', 
-        'is_confirmed', 'balance'
+    # Убираем упоминания о username
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Персональная информация', {'fields': ('first_name', 'last_name')}),
+        ('Права доступа', {
+            'fields': ('is_active', 'is_staff', 'is_admin', 'is_superuser', 'is_confirmed'),
+        }),
+        ('Важные даты', {'fields': ('last_login', 'date_joined')}),
+        ('Дополнительно', {'fields': ('balance',)}),
     )
+    
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'first_name', 'last_name', 'password1', 'password2'),
+        }),
+    )
+    
+    list_display = ('email', 'first_name', 'last_name', 'is_staff', 'is_admin', 'is_active')
+    list_filter = ('is_admin', 'is_staff', 'is_active')
     search_fields = ('email', 'first_name', 'last_name')
-    ordering = ('email',)
-    list_filter = ('is_admin', 'is_confirmed')
+    ordering = ('-date_joined',)
+    filter_horizontal = ()
+
+admin.site.register(User, CustomUserAdmin)
--- server/djangoapp/users/models.py
+++ server/djangoapp/users/models.py
@@ -6,18 +6,13 @@ from django.contrib.auth.models import (
 )
 from django.utils import timezone
 
-
-# Create your models here.
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
-        """Создаёт и сохраняет пользователя с указанным email и паролем"""
         if not email:
             raise ValueError('Email обязателен для регистрации')
         
         email = self.normalize_email(email)
-        
         user = self.model(email=email, **extra_fields)
-        
         user.set_password(password)
         user.save(using=self._db)
         return user
@@ -26,28 +21,25 @@ class UserManager(BaseUserManager):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
         extra_fields.setdefault('is_admin', True)
+        extra_fields.setdefault('is_active', True)
 
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError("Суперпользователь должен иметь is_staff=True.")
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError("Суперпользователь должен иметь is_superuser=True.")
-        
         return self.create_user(email, password, **extra_fields)
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-    """Кастомная модель пользователя"""
     first_name = models.CharField(max_length=30, verbose_name='Имя')
     last_name = models.CharField(max_length=30, verbose_name='Фамилия')
     email = models.EmailField(unique=True, verbose_name='Email')
-    password = models.CharField(max_length=128, verbose_name='Пароль')
     
     balance = models.IntegerField(default=0, verbose_name='Баланс ЛЭТИ-коинов')
     
-    is_confirmed = models.BooleanField(default=False, verbose_name='Почта подтверждена')
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False, verbose_name='Администратор')
+    is_confirmed = models.BooleanField(default=False, verbose_name='Почта подтверждена')
     
-    created_at = models.DateTimeField(default=timezone.now, verbose_name='Дата регистрации')
+    date_joined = models.DateTimeField(default=timezone.now, verbose_name='Дата регистрации')
+    last_login = models.DateTimeField(null=True, blank=True)
     
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['first_name', 'last_name']
@@ -57,16 +49,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     class Meta:
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
-        ordering = ['-created_at']
+        ordering = ['-date_joined']
 
-    def get_full_name(self):
-        """Возвращает полное имя пользователя."""
-        return f'{self.first_name} {self.last_name}'
-    
     def __str__(self):
         return f'{self.first_name} {self.last_name} ({self.email})'
-    
-    @property
-    def is_staff(self):
-        """Определяет, является ли пользователь сотрудником."""
-        return self.is_admin
